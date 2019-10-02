da5a2daede85686f56fca5471d89b01081b1f1a8
KJ Wilson <yourboykjdev@gmail.com>
2019-10-01 23:59:35 -0700

    work

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ app.use(jwt());
 app.use('/users', require('./users/users.controller'));
 
 // global error handler
-app.use(errorHandler);
+// app.use(errorHandler);
 
 // start server
 const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
